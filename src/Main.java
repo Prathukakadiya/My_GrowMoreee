c4b67074e0cbc9e07b5036dacbd7f39c328e73d4
kakadiya pratham <prathukakadiya7x@gmaiil.com>
2025-07-17 18:34:31 +0530

    passemail confirmation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import Model.User;
 import Service.*;
+import DB.*;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.HashMap;
@@ -12,31 +13,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         UserService userService = new UserService();
         TradingService tradingService = new TradingService();
+        DBConnection db = new DBConnection();
         StockService stockService = new StockService();
         UserValidation uv = new UserValidation();
-        HashMap<String,String> MailPass = new HashMap<>();
+        Connection con = db.getConnection();
+        HashMap<String, String> MailPass = new HashMap<>();
         while (true) {
             System.out.println("1. Register\n2. Login\n3.exit");
             int choice = sc.nextInt();
             sc.nextLine();
             User user = null;
 
-            switch (choice){
+            switch (choice) {
                 case 1:
-                uv.UserInput();
-                if (uv.age < 18) {
-                    System.out.println("You are not eligible for Trading!!");
-                    return;
-                }
-                if (userService.register(uv.name, uv.password, uv.aadhar, uv.pan, uv.email)) {
-                    MailPass.put(uv.email,uv.password);
-                    //System.out.println(Mail);
-                    System.out.println("Registered successfully.");
-                } else {
-                    System.out.println("Username already exists.");
-                    System.out.println("Now, Please Log In");
-                }
-                break;
+                    uv.UserInput();
+                    if (uv.age < 18) {
+                        System.out.println("You are not eligible for Trading!!");
+                        return;
+                    }
+                    if (userService.register(uv.name, uv.password, uv.aadhar, uv.pan, uv.email)) {
+                        MailPass.put(uv.email, uv.password);
+                        //System.out.println(Mail);
+                        System.out.println("Registered successfully.");
+                    } else {
+                        System.out.println("Username already exists.");
+                        System.out.println("Now, Please Log In");
+                    }
+                    break;
                 case 2:
                     String email;
                     boolean isValidmail;
@@ -60,73 +63,78 @@ public class Main {
                             System.out.println("Invalid email! Enter a correct email.");
                         }
                     } while (!isValidmail);
-                    if(!(MailPass.containsKey(email))){
-                    System.out.println("Email not found");
-                    System.out.println("Please Sign Up And Then Log In");
-                    break;
-                    }else{
+                    String q1 = "select Mail_id from users where Mail_id=?";
+                    PreparedStatement pst = con.prepareStatement(q1);
+                    pst.setString(1, email);
+                    ResultSet rs = pst.executeQuery();
+                    if (!(rs.next())) {
+                        System.out.println("Email not found");
+                        System.out.println("Please Sign Up And Then Log In");
+                        break;
+                    } else {
                         System.out.println("email found please enter your pass");
                         String pass = sc.next();
-                        if(MailPass.get(email).equals(pass)){
-                            System.out.println("log in successfully");
+                        String q2 = "select password from users where Mail_id=?";
+                        PreparedStatement pst2 = con.prepareStatement(q2);
+                        pst2.setString(1, email);
+                        ResultSet rs2 = pst2.executeQuery();
+                        if ((rs2.next())) {
+                            String actualPass = rs2.getString("password");
+                            if (actualPass.equals(pass)) {
+                                System.out.println("Login successfully");
+                                while (true) {
+                                    System.out.println("\n--- MENU ---");
+                                    System.out.println("1. View Stocks");
+                                    System.out.println("2. Buy Stock");
+                                    System.out.println("3. Portfolio");
+                                    System.out.println("4. Transaction History");
+                                    System.out.println("5. View All Logged-in Users");
+                                    System.out.println("6. Exit");
+
+                                    int opt = sc.nextInt();
+                                    sc.nextLine();
+
+                                    switch (opt) {
+                                        case 1:
+                                            for (var s : stockService.getAllStocks()) {
+                                                System.out.println(s.symbol + " - " + s.name + " - ₹" + s.price);
+                                            }
+                                            break;
+                                        case 2:
+                                            System.out.print("Enter symbol: ");
+                                            String sym = sc.nextLine();
+                                            System.out.print("Quantity: ");
+                                            int qty = sc.nextInt();
+                                            sc.nextLine();
+                                            if (tradingService.buyStock(user, sym, qty)) {
+                                                System.out.println("Success!");
+                                            } else {
+                                                System.out.println("Failed: insufficient funds or invalid stock.");
+                                            }
+                                            break;
+                                        case 3:
+                                            user.showPortfolio();
+                                            break;
+                                        case 4:
+                                            user.transactionHistory.display();
+                                            break;
+                                        case 5:
+                                            userService.showAllUsers();
+                                            break;
+                                        case 6:
+                                            System.out.println("Bye!");
+                                            return;
+                                    }
+                                }
+                            } else {
+                                System.out.println("pass wrong");
+                            }
                         }
+
                     }
 
+//
             }
-
-            /*while (user == null) {
-                System.out.print("Username: ");
-                String u = sc.nextLine();
-                System.out.print("Password: ");
-                String p = sc.nextLine();
-                user = userService.login(u, p);
-                if (user == null) System.out.println("Login failed. Try again.");
-            }*/
-
-//            while (true) {
-//                System.out.println("\n--- MENU ---");
-//                System.out.println("1. View Stocks");
-//                System.out.println("2. Buy Stock");
-//                System.out.println("3. Portfolio");
-//                System.out.println("4. Transaction History");
-//                System.out.println("5. View All Logged-in Users");
-//                System.out.println("6. Exit");
-//
-//                int opt = sc.nextInt();
-//                sc.nextLine();
-//
-//                switch (opt) {
-//                    case 1:
-//                        for (var s : stockService.getAllStocks()) {
-//                            System.out.println(s.symbol + " - " + s.name + " - ₹" + s.price);
-//                        }
-//                        break;
-//                    case 2:
-//                        System.out.print("Enter symbol: ");
-//                        String sym = sc.nextLine();
-//                        System.out.print("Quantity: ");
-//                        int qty = sc.nextInt();
-//                        sc.nextLine();
-//                        if (tradingService.buyStock(user, sym, qty)) {
-//                            System.out.println("Success!");
-//                        } else {
-//                            System.out.println("Failed: insufficient funds or invalid stock.");
-//                        }
-//                        break;
-//                    case 3:
-//                        user.showPortfolio();
-//                        break;
-//                    case 4:
-//                        user.transactionHistory.display();
-//                        break;
-//                    case 5:
-//                        userService.showAllUsers();
-//                        break;
-//                    case 6:
-//                        System.out.println("Bye!");
-//                        return;
-//                }
-//            }
         }
     }
 }
