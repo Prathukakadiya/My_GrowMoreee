9f0fdd25e97e7b576aaf50d51988eefdc9fc1fea
Prathukakadiya <prathukakadiya7x@gmail.com>
2025-08-20 22:05:50 +0530

    Update UserService.java

--- src/Service/UserService.java
+++ src/Service/UserService.java
@@ -7,6 +7,8 @@ import java.sql.*;
 import java.util.HashMap;
 
 public class UserService {
+
+    // Adding user registration details into database
     private final HashMap<String, User> userMap = new HashMap<>();
     public boolean register(String username, String password, String aadhar, String pan, String mail) throws SQLException {
         Connection con = DBConnection.getConnection();
@@ -21,39 +23,5 @@ public class UserService {
         int rows = ps.executeUpdate();
         con.close();
         return rows > 0;
-    }
-
-//    public User login(String username, String password) throws SQLException {
-//        if (userMap.containsKey(username)) return userMap.get(username);
-//
-//        Connection con = DBConnection.getConnection();
-//        PreparedStatement ps = con.prepareStatement(
-//                "SELECT * FROM users WHERE username=? AND password=?"
-//        );
-//        ps.setString(1, username);
-//        ps.setString(2, password);
-//        ResultSet rs = ps.executeQuery();
-//
-//        if (rs.next()) {
-//            User user = new User(
-//                    rs.getInt("id"),
-//                    username,
-//                    password,
-//                    rs.getDouble("balance")
-//            );
-//            userMap.put(username, user);
-//            return user;
-//        }
-//
-//        con.close();
-//        return null;
-//    }
-
-
-    public void showAllUsers() {
-        System.out.println("All Logged-in Users:");
-        for (String username : userMap.keySet()) {
-            System.out.println("- " + username);
-        }
     }
 }
