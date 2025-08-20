e84d2bcc7bb0d6a6aebdb5034b1dfb94101d8f5f
Prathukakadiya <prathukakadiya7x@gmail.com>
2025-08-20 22:05:14 +0530

    Update StockService.java

--- src/Service/StockService.java
+++ src/Service/StockService.java
@@ -1,59 +1,44 @@
 package Service;
 
+import DS.StocksList;
 import DB.DBConnection;
 import Model.Stock;
-
 import java.sql.*;
 import java.util.*;
 
 public class StockService {
-    public List<Stock> getAllStocks() throws SQLException {
+    Scanner sc = new Scanner(System.in);
+    StocksList stocksList = new StocksList();
+
+        // Fetch stocks details from Database
+    public void getAllStocks() throws SQLException {
         Connection con = DBConnection.getConnection();
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM stocks");
 
-        List<Stock> list = new ArrayList<>();
         while (rs.next()) {
-            Stock stock = new Stock(
-                    rs.getString("Symbols"),
-                    rs.getString("Company_names"),
-                    rs.getDouble("Previous_ClosePrice"),
-                    rs.getDouble("Today_OpenPrice")
-            );
-            list.add(stock);
+            String sym = rs.getString("Symbols");
+            String name = rs.getString("Company_names");
+            String category = rs.getString("Category");
+            double prev = rs.getDouble("Previous_ClosePrice");
+            double today = rs.getDouble("Today_OpenPrice");
 
-            // ✅ Pad and print here
-            String symbol = TableShow(stock.symbol, 12);
-            String name = TableShow(stock.name, 35);
-            String prev = "Previous Close: ₹" + stock.previousClosePrice;
-            String open = "Today Open: ₹" + stock.todayOpenPrice;
-
-            System.out.println(symbol + name + prev + "   |   " + open);
+            stocksList.InsertStocks(sym, name, category, prev, today);
         }
-        return list;
+        stocksList.DisplayStock();
+
+        rs.close();
+        st.close();
+        con.close();
     }
 
-    public String TableShow(String str, int length) {
+        // regex
+    public static String TableShow(String str, int length) {
         while (str.length() < length) {
             str += " ";
         }
         return str;
     }
-
-    public Stock getStock(String symbol) throws SQLException {
-        Connection con = DBConnection.getConnection();
-        PreparedStatement ps = con.prepareStatement("SELECT * FROM stocks WHERE symbol=?");
-        ps.setString(1, symbol);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-            return new Stock(
-                    rs.getString("Symbols"),
-                    rs.getString("Company_names"),
-                    rs.getDouble("Previous_ClosePrice"),
-                    rs.getDouble("Today_OpenPrice")
-            );
-        }
-        con.close();
-        return null;
-    }
+    
 }
+
